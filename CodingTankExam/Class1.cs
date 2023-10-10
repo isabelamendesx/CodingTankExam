91547f6a671d08e270c12bd219a1fd1735772a06
isabelamendesx <gomesisabela13@gmail.com>
2023-10-10 18:45:28 -0300

    Commit de teste

---
+++ CodingTankExam/Class1.cs
@@ -0,0 +1,53 @@
+﻿namespace CodingTankExam;
+
+public class Class1
+{
+    public static void Main()
+    {
+        do
+        {
+            Console.WriteLine("\nDeseja calcular novamente? (Digite 's' para sim ou qualquer outra tecla para sair)");
+                    
+        } while (Console.ReadLine().ToLower() == "s");
+        
+        
+    }
+    
+    public static double LerNumeroDouble(string prompt, int i)
+    {
+        double numero;
+        bool numValido = false;
+
+        do
+        {
+            Console.Write($"{prompt} {i}: ");
+            numValido = double.TryParse(Console.ReadLine(), out numero);
+
+            if (!numValido)
+            {
+                Console.WriteLine("Você não digitou um número válido. Tente novamente.\n");
+            }
+        } while (!numValido);
+
+        return numero;
+    }
+    
+    public static int LerNumeroInt(string prompt)
+    {
+        int numero;
+        bool numValido = false;
+
+        do
+        {
+            Console.Write($"{prompt}: ");
+            numValido = int.TryParse(Console.ReadLine(), out numero);
+
+            if (!numValido)
+            {
+                Console.WriteLine("Você não digitou um número válido. Tente novamente\n.");
+            }
+        } while (!numValido);
+
+        return numero;
+    }
+}
