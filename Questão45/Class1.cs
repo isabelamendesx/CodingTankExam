49b0ffb648916f3b6e07d69e8f585b161e231809
isabelamendesx <gomesisabela13@gmail.com>
2023-10-10 21:57:53 -0300

    Commit final

---
+++ Questão45/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Questão45;
+
+public class Class1
+{
+    public static void Main()
+    {
+        /*Int32 numero;
+        numero = Convert.ToInt16(!(21 > 21));
+        Console.WriteLine(numero); */
+        
+        for (int i = -5; i <= 7; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+        
+    /*
+     * QUESTAO 5 - ALTERNATIVA E O código imprimirá os números -5, -2, 1, 4 e 7. Ele começa em -5 e adiciona 3 a cada passo até atingir ou ultrapassar 7.
+     * QUESTAO 4 - O código exibirá o valor "1" no terminal. Isso ocorre porque a expressão !(21 > 21) é true, que, quando convertida em Int16, torna-se 1.
+     */
+
+}
